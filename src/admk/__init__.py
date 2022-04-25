d897c4fb720f1acc06e39af3ab1000e46b2c42d5
enricofacca <enrico.facca@sns.it>
2022-04-25 11:50:10 +0200

    working code with implicit scheme

--- src/admk/__init__.py
+++ src/admk/__init__.py
@@ -1,1 +1,7 @@
 __version__ = '0.0.0'
+
+from .admk_graph import Graph
+from .admk_graph import MinNorm
+from .admk_graph import TdensPotentialVelocity
+from .admk_graph import AdmkControls
+from .admk_graph import AdmkSolver
